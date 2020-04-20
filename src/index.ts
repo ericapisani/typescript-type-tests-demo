69172f48026b19ffdead5b16637138ca675c8e3f
Erica Pisani <erica.pisani@lever.co>
2020-04-20 16:34:46 -0400

    Complete initial functionality and standard test cases

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-function add(a: Number, b: Number): Number;
-function add(a: String, b: String): String;
+export function add(a: Number, b: Number): Number;
+export function add(a: String, b: String): String;
 
-function add(a: Number | String, b: Number | String): Number | String {
-  return 42;
+export function add(a: Number | String, b: Number | String): Number | String {
+  const isAAString = Object.prototype.toString.call(a) === "[object String]";
+  const isBAString = Object.prototype.toString.call(b) === "[object String]";
+
+  if (isAAString && isBAString) {
+    return `${a}${b}`;
+  } else {
+    return Number(a) + Number(b);
+  }
 }
--- tests/add_tsd_demo.test.ts
+++ tests/add_tsd_demo.test.ts
@@ -1,5 +1,15 @@
+import { add } from "../src/index";
+
 describe("add", () => {
   it("evaluates to true", () => {
     expect(true).toBe(true);
+  });
+
+  it("returns the addition of 2 numbers", () => {
+    expect(add(4, 2)).toEqual(6);
+  });
+
+  it("returns a concatenated string", () => {
+    expect(add("foo", "bar")).toEqual("foobar");
   });
 });
